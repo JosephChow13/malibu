b56309d93c0b000e76221090b7290fd9631cf089
Deokumarjnu <deo.kumar.jnu@gmail.com>
2018-09-07 14:16:39 +0530

    added crime, politics and latest section, need to make sepaat component for sidebar

---
+++ app/isomorphic/components/collection-templates/three-col-layout-grid.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {SectionHeading} from '../cards/section-heading';
+import {CardImageOnTop} from '../cards/card-image-on-top';
+import {SideImageCard} from '../cards/side-image-card';
+import {Headline} from '../cards/headlines';
+import "./three-col-layout-grid.m.css";
+
+const ThreeColLayout = (props) => {
+  const stories = props.stories.items[0].items;
+  return (
+    <div className="container">
+      <div styleName="cards">
+        <div styleName="crimeAndPolitics">
+          <div styleName="crime">
+            <SectionHeading heading="Crime"/>
+            <CardImageOnTop modifier="big" story={stories[0].story} getImageUrl={props.getImageUrl}/>
+          </div>
+          <div styleName="politics">
+            <SectionHeading heading="Politics"/>
+            <CardImageOnTop modifier="big" story={stories[4].story} getImageUrl={props.getImageUrl}/>
+          </div>
+        </div>
+        <div styleName="latest">
+          <SectionHeading heading="Latest"/>
+          {stories.slice(3,11).map(({story,id}) => {
+            return <SideImageCard story={story} key={id} getImageUrl={props.getImageUrl} sectionCard="latest"/>
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export {ThreeColLayout}
