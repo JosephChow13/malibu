16f3f43c6b2c3625a8b9cf663330efd993167f40
Deokumarjnu <deo.kumar.jnu@gmail.com>
2018-09-07 14:26:55 +0530

    added heading component on crime and politics section

--- app/isomorphic/components/cards/headlines.js
+++ app/isomorphic/components/cards/headlines.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './headline.m.css';
 
 const Headline = ({story}) => {
-  console.log(story);
   return <div styleName="headingWrapper"><h2 styleName="headlines">{story.headline}</h2></div>;
 }
 
--- app/isomorphic/components/collection-templates/three-col-layout-grid.js
+++ app/isomorphic/components/collection-templates/three-col-layout-grid.js
@@ -14,10 +14,16 @@ const ThreeColLayout = (props) => {
           <div styleName="crime">
             <SectionHeading heading="Crime"/>
             <CardImageOnTop modifier="big" story={stories[0].story} getImageUrl={props.getImageUrl}/>
+            <Headline story={stories[1].story}/>
+            <Headline story={stories[2].story}/>
+            <Headline story={stories[3].story}/>
           </div>
           <div styleName="politics">
             <SectionHeading heading="Politics"/>
             <CardImageOnTop modifier="big" story={stories[4].story} getImageUrl={props.getImageUrl}/>
+            <Headline story={stories[5].story}/>
+            <Headline story={stories[6].story}/>
+            <Headline story={stories[7].story}/>
           </div>
         </div>
         <div styleName="latest">
