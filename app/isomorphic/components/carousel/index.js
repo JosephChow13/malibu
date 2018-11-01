ea155b8a8195b92bd2b5a06178963c2cdcdc5b9e
Tejas Dinkar <tejas@gja.in>
2018-11-01 17:50:28 +0530

    Fixing a minify issue

--- app/isomorphic/components/carousel/index.js
+++ app/isomorphic/components/carousel/index.js
@@ -1,2 +1,17 @@
 import "./carousel.m.css";
-export { Carousel } from "react-responsive-carousel";
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+export default function DefaultCarousel(props) {
+  return (
+    <Carousel
+      showThumbs={false}
+      showStatus={false}
+      showArrows
+      autoPlay
+      emulateTouch
+      infiniteLoop
+      {...props}
+    />
+  );
+}
--- app/isomorphic/components/collection-templates/fullscreen-slider/index.js
+++ app/isomorphic/components/collection-templates/fullscreen-slider/index.js
@@ -1,38 +1,28 @@
 import React from "react";
-import { Carousel } from "../../carousel";
+import Carousel from "../../carousel";
 import { arrayOf, object } from "prop-types";
 import { Link, ResponsiveHeroImage } from "@quintype/components";
 import "./fs-slider.m.css";
 
-export function FullscreenSlider({ stories }) {
-  return (
-    <Carousel
-      showThumbs={false}
-      showStatus={false}
-      showArrows
-      autoPlay
-      emulateTouch
-      infiniteLoop
-      styleName="carousel"
-    >
-      {stories.map((story, index) => (
-        <Link
-          key={`${index}-${story.id}`}
-          href={`/${story.slug}`}
-          styleName="story"
-        >
-          <ResponsiveHeroImage
-            story={story}
-            aspectRatio={[16, 9]}
-            defaultWidth={968}
-            widths={[320, 640, 968]}
-            styleName="hero"
-          />
-        </Link>
-      ))}
-    </Carousel>
-  );
-}
+export const FullscreenSlider = ({ stories }) => (
+  <Carousel styleName="carousel">
+    {stories.map((story, index) => (
+      <Link
+        key={`${index}-${story.id}`}
+        href={`/${story.slug}`}
+        styleName="story"
+      >
+        <ResponsiveHeroImage
+          story={story}
+          aspectRatio={[16, 9]}
+          defaultWidth={968}
+          widths={[320, 640, 968]}
+          styleName="hero"
+        />
+      </Link>
+    ))}
+  </Carousel>
+);
 
 FullscreenSlider.propTypes = {
   stories: arrayOf(object)
